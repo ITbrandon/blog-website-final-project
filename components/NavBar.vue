1ac529bda36083988332ee0106a1e028cbe1c95d
Itbrandon <brandoniticka@gmail.com>
2024-04-02 18:01:01 -0700

    update

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,12 +1,26 @@
 <script setup>
 const response = await useFetch('/api/links');
 const linkData = toRaw(response.data.value);
+const user = useSupabaseUser();
+
+const email = () => {
+  if(!user.value)
+  {
+    return "Email";
+  }
+  else
+  {
+    return user.value.email;
+  }
+}
+
 </script>
 
 <template>
 <nav>
 <ul>
   <li v-for="({title, href},i) in linkData" :key="i"><NuxtLink :to="href">{{ title }}</NuxtLink></li>
+  <li>{{ email() }}</li>
 </ul>
 </nav>
 </template>
